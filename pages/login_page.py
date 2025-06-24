dab4440f02f19ae8650f765b61e4ea44e9d2cb23
Dreusus <pitakxdd@gmail.com>
2025-06-24 13:09:02 +0300

    Описана страница авторизации

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,10 +1,24 @@
+from elements.button import Button
 from elements.input import Input
 from pages.base_page import BasePage
 from playwright.sync_api import Page
 
+
 class LoginPage(BasePage):
 
-    def __init__(self,page:Page):
+    def __init__(self, page: Page):
         super().__init__(page)
-        self.login_input = Input(page,"//input[@id='sdo-login']",'login')
-        self.password_input = Input(page,"//input[@id='sdo-password']",'password')
+        self.username_input = Input(page, "//input[@id='sdo-login']", 'login')
+        self.password_input = Input(page, "//input[@id='sdo-password']", 'password')
+        self.login_button = Button(page, "//button/span[text()='Войти']", "Войти")
+        self.recovery_button = Button(page, "//button/span[text()='Забыли пароль?']", "Забыли пароль?")
+
+    def fill(self, username, password):
+        self.username_input.fill(username)
+        self.password_input.fill(password)
+
+    def click_login_button(self):
+        self.login_button.click()
+
+    def click_recovery_button(self):
+        self.recovery_button.click()
